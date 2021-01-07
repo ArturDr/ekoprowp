56075b07e8af13c1c525162ef18a55925535f1b0
Artur Robek <robek.artur@gmial.com>
2021-01-07 14:58:01 +0100

    add 'realizacje'; add map in footer style both

--- .history/wp-content/themes/wparekopro/footer_20210107144535.php
+++ .history/wp-content/themes/wparekopro/footer_20210107144535.php
@@ -1,22 +1,22 @@
         <!-- sec4-->
         <section id="sec4">
             <div class="container">
-                <div class="row" id="kontakt">
-                    <div class="col">
+                <div class="row justify-content-end" id="kontakt">
+                    <div class="col-md-5 col-12">
                         <h1 class="text-center">KONTAKT</h1>
                     </div>
                 </div>
-                <div class="row justify-content-center">
-                    <div class="col-md-6 col-12 text-center">
-                        <p>ul. Rudzka 96/2, 44-200 Rybnik</p>
-                        <p><a href="tel:+48 601 214 021">+48 601 214 021</a></p>
-                        <p><a href="mailto:robek.roman@gmail.com">robek.roman@gmail.com</a></p>
-                        <p>BHP: <a href="mailto:robek.bhp@gmail.com">robek.bhp@gmail.com</a></p>
-                        <p>NIP: 642 041 31 14</p>
-                        <p>Regon: 241644630</p>
+                <div class="row justify-content-evenly">
+                    <div class="col-md-5 col-12 text-center">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10235.209911355048!2d18.535915!3d50.108704!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa8fe8ea04630f6be!2sEkoprojekt%20Roman%20Robek%20-%20budownictwo%2C%20specjalista%20ds.%20BHP%20i%20zarz%C4%85dzania%20%C5%9Brodowiskiem!5e0!3m2!1spl!2spl!4v1610026528238!5m2!1spl!2spl" width="300" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
                     </div>
-                    <div class="col-md-6 col-12 text-center">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10235.209911355048!2d18.535915!3d50.108704!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa8fe8ea04630f6be!2sEkoprojekt%20Roman%20Robek%20-%20budownictwo%2C%20specjalista%20ds.%20BHP%20i%20zarz%C4%85dzania%20%C5%9Brodowiskiem!5e0!3m2!1spl!2spl!4v1610026528238!5m2!1spl!2spl" width="300" height="240" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    <div class=" col-md-5 col-12">
+                        <p class="text-center">ul. Rudzka 96/2, 44-200 Rybnik</p>
+                        <p class="text-center"><a href="tel:+48 601 214 021">+48 601 214 021</a></p>
+                        <p class="text-center"><a href="mailto:robek.roman@gmail.com">robek.roman@gmail.com</a></p>
+                        <p class="text-center">BHP: <a href="mailto:robek.bhp@gmail.com">robek.bhp@gmail.com</a></p>
+                        <p class="text-center">NIP: 642 041 31 14</p>
+                        <p class="text-center">Regon: 241644630</p>
                     </div>
                 </div>
                 <div class="row justify-content-end">
---
+++ .history/wp-content/themes/wparekopro/page-realiz_20210107142308.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Ekoprojekt
+ * Template Name: Realizacje
+ *
+ */
+
+ // Advanced Custom Fields
+$tytul_strony = get_field('tytul_strony');
+$tresc = get_field('tresc');
+
+
+get_header();
+?>
+
+<section id="sec1_bhp">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-12">
+				<h1><?php echo $tytul_strony ?></h1>
+                <p><?php echo $tresc ?></p>
+			</div>
+        </div>
+    </div>
+</section>
+<section id="sec2_realiz">
+	<div class="container">
+        <div class="row justify-content-center">
+			<div class="col-md-3 col-12">
+                <iframe width="400" height="300" src="https://www.youtube.com/embed/vBFB0O7bZpI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="col-md-3 col-12">
+                <iframe width="400" height="300" src="https://www.youtube.com/embed/vD4cHwWhHYY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="col-md-3 col-12">
+
+			</div>
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer();
+?>
