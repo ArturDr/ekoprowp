5980247f3e123f95c12cf26acfe8e39926ad3ede
Artur Robek <robek.artur@gmial.com>
2021-01-07 17:37:32 +0100

    debug footer and header

---
+++ .history/wp-content/themes/wparekopro/content_20210107173546.php
@@ -0,0 +1,16 @@
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <article>
+                <h2><?php the_title(); ?></h2>
+                <div class="info">
+		        <span><?php the_author(); ?></span> | <span><?php the_time('d-m-Y'); ?></span>
+	        </div>
+                <?php the_excerpt(); ?>
+                <a class="more" href="<?php the_permalink(); ?>"> Czytaj więcej </a>
+        </article>
+    <?php endwhile; ?>
+<?php else: ?>
+        <h2>Nic nie znaleziono.</h2>
+<?php endif; ?>
+<div class="navigation-bottom">
+        <?php next_posts_link('<< Starsze artykuły'); ?> | <?php previous_posts_link('Nowsze artykuły >>'); ?>
+</div>
